465e2d1c78cdad8c06ac01507cdccc9b465e5ceb
s-razoes <s.razoes+github@gmail.com>
2023-02-01 12:53:27 +0000

    added console control

--- wserver.py
+++ wserver.py
@@ -1,20 +1,34 @@
 import threading
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer
-
+import json
 WS_PORT = '8091'
 
 clients = []
+servers = []
 
+properties = {}
 
 class Handler(WebSocket):
     def handleMessage(self):
         print('Got message: ' + self.data)
-        for client in clients:
-            if client != self:
-                client.sendMessage(self.data)
+        if self.data == 'status':
+            self.sendMessage(json.dumps(properties))
+            servers.append(self)
+        else:
+            obj = json.loads(self.data)
+            if 'action' in obj:
+                properties[obj['action']] = obj['num']
+            for client in clients:
+                if client != self:
+                    if client in servers:
+                        self.sendMessage(json.dumps(properties))
+                    else:
+                        client.sendMessage(self.data)
 
     def handleConnected(self):
-        print('Connected:' + str(self))
+        print('Connected:' + str(self), end='')
+        #clients.sendMessage(json.dumps(properties))
+        print(':> ' + json.dumps(properties))
         clients.append(self)
 
     def handleClose(self):
